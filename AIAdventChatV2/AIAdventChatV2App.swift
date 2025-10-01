bb641ddd0a1310d9f23d0908916355cc293abfa7
Sergey Markov <msergei2008@gmail.com>
2025-10-01 21:57:53 +0500

    Initial Commit

---
+++ AIAdventChatV2/AIAdventChatV2App.swift
@@ -0,0 +1,17 @@
+//
+//  AIAdventChatV2App.swift
+//  AIAdventChatV2
+//
+//  Created by Sergey Markov on 01.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct AIAdventChatV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
